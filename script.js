75d3089b83b5e10cbd79ae4ef6b1aac9cc7d3dac
Nyot-Nyot <dzakisultan012@gmail.com>
2025-02-20 23:07:54 +0700

    Modularize game function
    added getPlayerChoice and playRound functions

--- script.js
+++ script.js
@@ -10,36 +10,33 @@ function getComputerChoice() {
     }
 }
 
-function determineWinner(playerChoice, computerChoice) {
-    if (playerChoice == computerChoice) {
-        return "It's a tie!";
-    } else if (
-        (playerChoice == "rock" && computerChoice == "scissors") ||
-        (playerChoice == "paper" && computerChoice == "rock") ||
-        (playerChoice == "scissors" && computerChoice == "paper")
-    ) {
-        return "You win!";
-    } else {
-        return "You lose!";
+function getPlayerChoice(event) {
+    const card = event.target.closest(".card");
+
+    if (card) {
+        return card.getAttribute("data-choice");
     }
+
+    return null;
 }
 
-let playerScore = 0;
-let totalGames = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-document.getElementById("choices").addEventListener("click", function(event) {
-    if (event.target.closest(".card")) {
-        let playerChoice = event.target.closest(".card").getAttribute("data-choice");
-        let computerChoice = getComputerChoice();
-        let result = determineWinner(playerChoice, computerChoice);
-
-        alert(`You chose ${playerChoice}. The computer chose ${computerChoice}. ${result}`);
-
-        if (result == "You win!") {
-            playerScore++;
-        }
-        totalGames++;
-
-        document.querySelector(".score").textContent = `Score: ${playerScore}/${totalGames}`;
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return "It's a tie!";
     }
-});
+
+    if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+        humanScore++;
+        return "You win!";
+    }
+
+    computerScore++;
+    return "You lose!";
+}
